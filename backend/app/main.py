e166abc78c49ebcbfca85e615a6ab4918f1db6f5
chiefzz7 <sr1882939@gmail.com>
2026-05-01 18:16:28 -0300

    feat(backend): implementa instancia inicial do fastapi no main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,14 @@
-print("Hello, AtipicTouch")
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="AtipicTouch API",
+    description="API para o ecossistema IoT da Atipic Touch",
+    version="1.0.0"
+)
+
+@app.get("/")
+def health_check():
+    return {
+        "status": "online",
+        "message": "Cérebro AtipicTouch rodando perfeitamente na nuvem!"
+    }
