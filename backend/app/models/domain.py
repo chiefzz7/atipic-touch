8555e785d3ce543a481e13bf4e0bee6728fb58df
chiefzz7 <sr1882939@gmail.com>
2026-05-09 19:50:20 -0300

    feat(backend): adiciona as tabelas FeedingLogs e SensoryFeedback no /model

--- backend/app/models/domain.py
+++ backend/app/models/domain.py
@@ -1,13 +1,13 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional, List
-from datetime import datetime
+from datetime import datetime, date
 from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import ARRAY
 
 class Usuario(SQLModel, table=True):
     __tablename__ = "usuarios"
 
-    id: str = Field(dafault=None, primary_key=True)
+    id: str = Field(primary_key=True)
     nome: str
     email: str
     telefone: str
@@ -18,10 +18,10 @@ class Usuario(SQLModel, table=True):
 class Crianca(SQLModel, table=True):
     __tablename__ = "criancas"
 
-    id: str = Field(default=None, primary_key=True)
+    id: str = Field(primary_key=True)
     usuarioId: str = Field(foreign_key="usuarios.id")
     nome: str
-    idade: str
+    datanascimento: date
     temasPreferidos: List[str] = Field(sa_column=Column(ARRAY(String)))
     restricoesMedicas: Optional[str] = None
 
@@ -37,3 +37,23 @@ class Alimento(SQLModel, table=True):
     sabor: str
     cheiro: str
     temperatura: str
+
+
+class FeedingLog(SQLModel, table=True):
+    __tablename__ = "feeding_logs"
+
+    id: str = Field(primary_key=True)
+    timestamp: datetime = Field(default_factory=datetime.utcnow)
+    usuarioId: str = Field(foreign_key="usuarios.id")
+    criancaId: str = Field(foreign_key="criancas.id")
+    alimentoId: int = Field(foreign_key="alimentos.id")
+    reacao: int
+    origem: str
+
+class SensoryFeedback(SQLModel, table=True):
+    __tablename__ = "sensory_feedbacks"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    feedingLogId: str = Field(foreign_key="feeding_logs.id")
+    atributo: str
+    gostou: bool
