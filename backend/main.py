67159dbef9869f447a10e5e192dd5f9c4334c526
chiefzz7 <sr1882939@gmail.com>
2026-05-09 22:04:33 -0300

    feat(auth): implementa geracao de token JWT e rota de login com mitigacao e enumeracao

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes import user_routes
+from app.routes import auth_routes
 
 app = FastAPI(
     title="AtipicTouch API",
@@ -8,6 +9,7 @@ app = FastAPI(
 )
 
 app.include_router(user_routes.router)
+app.include_router(auth_routes.router)
 
 @app.get("/")
 def health_check():
