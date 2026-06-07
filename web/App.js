004d5f04f8f935a264f49acb68a8977778b25707
chiefzz7 <sr1882939@gmail.com>
2026-06-07 13:32:42 -0300

    feat(web): implementa tela de cadastro com espelhamento visual para futura animacao

--- web/App.js
+++ web/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 import './global.css';
 
 export default function App() {
-  return <Login />;
+  return <Register />;
 }
