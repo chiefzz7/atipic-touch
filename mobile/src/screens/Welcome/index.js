5d0a04abc494734a385700a021ae7b74cd35c576
Ravel <ravel.melgaco1@gmail.coml>
2026-06-26 15:38:03 -0300

    style(mobile): refina welcome screen conforme protótipo do figma

--- mobile/src/screens/Welcome/index.js
+++ mobile/src/screens/Welcome/index.js
@@ -8,57 +8,59 @@ export default function WelcomeScreen() {
   return (
     <View className="flex-1 bg-[#FFFCEF] overflow-hidden">
 
-      {/* Círculo bege */}
-      <View
-        className="absolute rounded-full bg-[#E5D8B0]"
-        style={{
-          width: 713,
-          height: 756,
-          left: -148,
-          top: -20,
-        }}
-      />
+  {/* Círculo */}
+  <View
+    className="absolute rounded-full bg-[#E5D8B0]"
+    style={{
+      width: 713,
+      height: 756,
+      left: -148,
+      top: 0,
+    }}
+  />
 
-      {/* Conteúdo */}
-      <View className="flex-1 items-center">
-
-        {/* Mascote */}
-        <Image
+  {/* Mascote */}
+  <Image
     source={require("../../../assets/images/welcome_brocolis.png")}
     resizeMode="contain"
-    className="absolute"
+    className="absolute rounded-full"
     style={{
       width: 335,
-      height: 358,
+      height: 335,
       top: 143,
       left: 41,
+    }}
+  />
+
+  {/* Botão */}
+  <TouchableOpacity
+  className="absolute bg-[#D8C792] items-center justify-center rounded-[7px]"
+  style={{
+    width: 350,
+    height: 83,
+    left: 31,
+    top: 568,
   }}
-/>
+  onPress={() => router.push("/register")}
+>
+  <Text className="text-white text-[40px] font-heavy">
+    Criar minha conta
+  </Text>
+</TouchableOpacity>
 
-        {/* Área inferior */}
-        <View className="absolute bottom-24 items-center">
-
-          <TouchableOpacity
-            className="w-[350px] h-[83px] bg-[#D6C48F] rounded-[7px] items-center justify-center"
-            onPress={() => router.push("/register")}
-          >
-            <Text className="text-white text-[34px] font-light">
-              Criar minha conta
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            className="mt-5"
-            onPress={() => router.push("/login")}
-          >
-            <Text className="text-black text-[24px]">
-              Já tem conta? ENTRAR.
-            </Text>
-          </TouchableOpacity>
-
-        </View>
-
-      </View>
-    </View>
+{/* Login */}
+<TouchableOpacity
+  className="absolute items-center"
+  style={{
+    top: 655,
+    width: 412,
+  }}
+  onPress={() => router.push("/login")}
+>
+  <Text className="text-[25px] text-[#1E1E1E] font-light">
+    Já tem conta? <Text className="font-semibold">ENTRAR.</Text>
+  </Text>
+</TouchableOpacity>
+</View>
   );
 }
