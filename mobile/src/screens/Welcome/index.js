42e9d1187fa3d38abe885fe14c72b58355efb76c
Ravel3122 <ravel.melgaco1@gmail.com>
2026-08-10 16:19:23 -0300

    fix(mobile): centraliza elementos da tela de boas-vindas

--- mobile/src/screens/Welcome/index.js
+++ mobile/src/screens/Welcome/index.js
@@ -1,4 +1,3 @@
-
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import { useRouter } from "expo-router";
 
@@ -6,57 +5,41 @@ export default function WelcomeScreen() {
   const router = useRouter();
 
   return (
-    <View className="flex-1 bg-[#FFFCEF] overflow-hidden">
+    <View className="flex-1 items-center">
 
-  <View
-    className="absolute rounded-full bg-[#E5D8B0]"
-    style={{
-      width: 713,
-      height: 756,
-      left: -148,
-      top: 0,
-    }}
-  />
+      <View
+        className="absolute rounded-full bg-[#E5D8B0]"
+        style={{ width: 713, height: 756, top: 0 }}
+      />
 
-  <Image
-    source={require("../../../assets/images/welcome_brocolis.png")}
-    resizeMode="contain"
-    className="absolute rounded-full"
-    style={{
-      width: 335,
-      height: 335,
-      top: 143,
-      left: 41,
-    }}
-  />
+      <Image
+        source={require("../../../assets/images/welcome_brocolis.png")}
+        resizeMode="contain"
+        className="absolute"
+        borderRadius={335 / 2}
+        style={{ width: 335, height: 335, top: 143}}
+      />
 
-  <TouchableOpacity
-  className="absolute bg-[#D8C792] items-center justify-center rounded-[7px]"
-  style={{
-    width: 350,
-    height: 83,
-    left: 31,
-    top: 568,
-  }}
-  onPress={() => router.push("/register")}
->
-  <Text className="text-white text-[40px] font-heavy">
-    Criar minha conta
-  </Text>
-</TouchableOpacity>
+      <TouchableOpacity
+        className="absolute w-[85%] max-w-[350px] h-[83px] bg-[#D8C792] items-center justify-center rounded-[7px]"
+        style={{ top: 568 }}
+        onPress={() => router.push("/register")}
+      >
+        <Text className="text-white text-[24px] font-bold">
+          Criar minha conta
+        </Text>
+      </TouchableOpacity>
 
-<TouchableOpacity
-  className="absolute items-center"
-  style={{
-    top: 655,
-    width: 412,
-  }}
-  onPress={() => router.push("/login")}
->
-  <Text className="text-[25px] text-[#1E1E1E] font-light">
-    Já tem conta? <Text className="font-semibold">ENTRAR.</Text>
-  </Text>
-</TouchableOpacity>
-</View>
+      <TouchableOpacity
+        className="absolute items-center"
+        style={{ top: 655 }}
+        onPress={() => router.push("/login")}
+      >
+        <Text className="text-[#6E6246] text-[18px]">
+          Já tem uma conta? <Text className="font-bold">Entrar</Text>
+        </Text>
+      </TouchableOpacity>
+
+    </View>
   );
 }
