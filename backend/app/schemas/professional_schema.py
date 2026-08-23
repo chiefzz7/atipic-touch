a66d35852a33ef755a4e58167b286dc0111fd990
chiefzz7 <sr1882939@gmail.com>
2026-08-23 13:06:37 -0300

    fix: corrige erro ao buscar criancas

--- backend/app/schemas/professional_schema.py
+++ backend/app/schemas/professional_schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import List
+from datetime import date
 
 
 class ProfessionalPatientResponse(BaseModel):
@@ -11,8 +12,11 @@ class ProfessionalPatientResponse(BaseModel):
 class ProfessionalChildResponse(BaseModel):
     id: str
     nome: str
-    dataNascimento: str
+    dataNascimento: date
     temasPreferidos: List[str]
     restricoesMedicas: str | None
     usuarioId: str
-    
+
+    class Config:
+        from_attributes = True
+        
